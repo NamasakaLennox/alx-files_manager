d5b51c12e6ba9b7e63d023df7f2fcc94cff07819
NamasakaLennox <namasakalennox254@gmail.com>
2024-02-13 20:47:37 +0300

    [new]: implemented a simple database manager

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor () {
+    this.client = createClient();
+    this.isClientConnected = true;
+
+    this.client.on('error', (err) => {
+      this.isClientConnected = false;
+      console.log(err.message || err.toString());
+    });
+  }
+
+  isAlive () {
+    return this.isClientConnected;
+  }
+
+  async get (key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
+
+  async set (key, value, duration) {
+    await promisify(this.client.SETEX).bind(this.client)(key, duration, value);
+  }
+
+  async del (key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+const redisClient = new RedisClient();
+export default redisClient;
